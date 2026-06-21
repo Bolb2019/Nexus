3ffd4c13168413de79419989dfddb0ea9bcad6fe
Bolb2019 <zachnoahstein@gmail.com>
2026-06-21 13:26:11 -0700

    feat: find IP button

--- nexus/Menu/Lobby/server_lobby.gd
+++ nexus/Menu/Lobby/server_lobby.gd
@@ -12,3 +12,14 @@ func _on_exit_button_pressed() -> void:
 
 func _on_play_button_pressed() -> void:
 	Lobby.start_game.rpc($City.get_data())
+
+
+func _on_ip_pressed() -> void:
+	$VBoxContainer/Label.text = "IP: " + str(get_local_ip())
+
+func get_local_ip() -> String:
+	for address in IP.get_local_addresses():
+		if "." in address and not address.begins_with("127.") and not address.begins_with("169.254."):
+			if address.begins_with("192.168.") or address.begins_with("10.") or (address.begins_with("172.") and int(address.split(".")[1]) >= 16 and int(address.split(".")[1]) <= 31):
+				return address
+	return "127.0.0.1"
