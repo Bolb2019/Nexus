4533932c74d6c6711512a97c3e254a93fd923ebc
Bolb2019 <zachnoahstein@gmail.com>
2026-06-21 12:13:03 -0700

    fix: oops

--- nexus/Menu/menu.gd
+++ nexus/Menu/menu.gd
@@ -7,17 +7,15 @@ func _ready() -> void:
 	%Player/Camera2D.position_smoothing_enabled = false
 
 func _on_play_button_pressed() -> void:
-	if !GlobalStats.tutorial:
-		GlobalStats.score = 0
-		%Player/Label.visible = true
-		SceneManager.change_to_scene("client_lobby")
+	GlobalStats.score = 0
+	%Player/Label.visible = true
+	SceneManager.change_to_scene("client_lobby")
 
 func _on_quit_button_pressed() -> void:
 	if !GlobalStats.tutorial:
 		get_tree().quit()
 
 func _on_host_button_pressed() -> void:
-	if !GlobalStats.tutorial:
-		GlobalStats.score = 0
-		%Player/Label.visible = true
-		SceneManager.change_to_scene("server_lobby")
+	GlobalStats.score = 0
+	%Player/Label.visible = true
+	SceneManager.change_to_scene("server_lobby")
--- nexus/Player/Global_stats.gd
+++ nexus/Player/Global_stats.gd
@@ -1,6 +1,5 @@
 extends Node
 
-var score = 500
-
+var score := 500
 
 var map_data: Dictionary
